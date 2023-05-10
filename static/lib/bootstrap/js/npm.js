c9ce269296eb3b5da58923ebf29a6a79d9c20ba6
TXL pass through <59956776+xiaolongtongxue@users.noreply.github.com>
2023-05-10 15:52:18 +0800

    HTML前端静态文件

---
+++ static/lib/bootstrap/js/npm.js
@@ -0,0 +1,4 @@
+[ZoneTransfer]
+ZoneId=3
+ReferrerUrl=https://v3.bootcss.com/
+HostUrl=https://objects.githubusercontent.com/github-production-release-asset-2e65be/2126244/d2e19d80-2fb8-11e9-85b1-ef585ab03b84?X-Amz-Algorithm=AWS4-HMAC-SHA256&X-Amz-Credential=AKIAIWNJYAX4CSVEH53A%2F20230205%2Fus-east-1%2Fs3%2Faws4_request&X-Amz-Date=20230205T083702Z&X-Amz-Expires=300&X-Amz-Signature=fa774a10b7d12d0bc8909c110679a6ee97e5310bfff52daffb9b69ddf4f01761&X-Amz-SignedHeaders=host&actor_id=0&key_id=0&repo_
